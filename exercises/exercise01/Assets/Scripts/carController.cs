e1ee15c36e507f5f585d4cd4fe0bc81e24dd8a40
Kristen <kristen.marcinek@rutgers.edu>
2023-01-25 14:40:15 -0500

    add rube goldberg exercise 1

---
+++ exercises/exercise01/Assets/Scripts/carController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class carController : MonoBehaviour
+{
+    public Rigidbody rb;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.useGravity = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space)) {
+            rb.useGravity = true;
+        }
+
+    }
+}
